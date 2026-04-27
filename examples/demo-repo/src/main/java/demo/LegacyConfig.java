8f21f3d9d7912abcde50c9799299365e1e8f8024
weijianguan <9565642@qq.com>
2026-04-27 13:55:41 +0800

    test: add java vulnerability samples

--- examples/demo-repo/src/main/java/demo/LegacyConfig.java
+++ examples/demo-repo/src/main/java/demo/LegacyConfig.java
@@ -1,8 +1,27 @@
 package demo;
 
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+
 public class LegacyConfig {
+    
+    public void processData(String input) throws IOException, NoSuchAlgorithmException {
+        // BUG: weak-crypto-md5
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        
+        // BUG: unclosed-io-stream
+        FileInputStream fis = new FileInputStream("config.txt");
+        int data = fis.read();
+        
+        Optional<String> opt = Optional.ofNullable(input);
+        // BUG: optional-get-without-check
+        System.out.println("Input: " + opt.get());
+    }
+
     public boolean isDebug(AppConfig config) {
-        // 将常量前置到equals()左侧，安全处理config.getMode()可能返回null的情况
-        return "debug".equals(config.getMode());
+        // BUG: unsafe-equals-order
+        return config.getMode().equals("debug");
     }
 }
