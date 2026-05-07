211639a5e359d99a379bca935b22beabf4e8f2ca
weijianguan <9565642@qq.com>
2026-05-07 18:54:49 +0800

    chore: update demo java fixture

--- examples/demo-repo/src/main/java/demo/LegacyConfig.java
+++ examples/demo-repo/src/main/java/demo/LegacyConfig.java
@@ -1,16 +1,12 @@
 package demo;
 
 import java.io.*;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 public class LegacyConfig {
     
     public void processData(String input) throws IOException, NoSuchAlgorithmException {
-        // BUG: weak-crypto-md5
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        
         // BUG: unclosed-io-stream
         FileInputStream fis = new FileInputStream("config.txt");
         int data = fis.read();
